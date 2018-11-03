0ab54bbc56d84aea5780bea864b3a8b0930bf87b
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-11-03 02:30:55 +0200

    implement navigation bar style changing behaviour

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,25 @@
-const perfundo = require('perfundo');
-
 document.addEventListener("DOMContentLoaded", function () {
 
-  const hamburgerBtn = document.getElementById('hamburgerBtn')
-  const navList = document.getElementById('navList')
+  const hamburgerBtn = document.getElementById('hamburgerBtn');
+  const navList = document.getElementById('navList');
+  const nav = document.getElementsByClassName('nav')[0];
+  const navLis = document.getElementsByClassName('nav__li');
+  const navLogo = document.getElementsByClassName('nav__logo')[0];
 
   hamburgerBtn.addEventListener('click', function () {
     navList.classList.toggle('nav__ul--active');
+  })
 
-    perfundo('.perfundo', {
-      disableHistory: true,
-      swipe: true
-    });
+  window.addEventListener('scroll', function () {
+    var scrollPosY = window.pageYOffset | document.body.scrollTop;
+    if (scrollPosY > 150) {
+      nav.classList.remove('nav--no-background');
+      navLogo.classList.remove('nav__logo--no-color');
+      Array.from(navLis).map((elem) => elem.classList.remove('nav__li--no-color'));
+    } else if (scrollPosY <= 150) {
+      nav.classList.add('nav--no-background');
+      navLogo.classList.add('nav__logo--no-color');
+      Array.from(navLis).map((elem) => elem.classList.add('nav__li--no-color'));
+    }
   })
 })
